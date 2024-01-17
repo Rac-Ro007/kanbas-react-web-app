a2abe06e1589e831c78919215f035914e602d22f
Ronak Vadhaiya <ronak.vadhaiya@somaiya.edu>
2024-01-17 16:37:49 -0500

    home updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         
-      </header>
+      </header> */}
     </div>
   );
 }
