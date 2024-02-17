fe66ff8378551f4250f4b8318566edf1394667ca
Ronak Vadhaiya <ronak.vadhaiya@somaiya.edu>
2024-02-16 19:57:05 -0500

    Home screen development

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,12 +1,289 @@
 import ModuleList from "../Modules/List";
+import {FaFileImport, FaBullhorn, FaBullseye, FaChartSimple, FaBell} from "react-icons/fa6";
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
-      <ModuleList />
-      <h2>Status</h2>
+    <div className="d-flex">
+
+        <div className="flex-fill">
+            
+            <div className="row justify-content-end p-3">
+                <div className="col-auto">
+                    <button className="btn btn-light">Collapse All</button>
+                </div>
+                <div className="col-auto">
+                    <button className="btn btn-light">View Progress</button>
+                </div>
+                <div className="col-auto">
+                    <select className="form-select" aria-label="Default select example">
+                        <option>Publish All</option>
+                        <option>Unpublish All</option>
+                    </select>
+                </div>
+                <div className="col-auto">
+                    <button className="btn btn-danger">+ Modules</button>
+                </div>
+            </div>
+            <hr/>
+            {/* <ul className="list-group wd-modules p-4">
+                <li className="list-group-item">
+                    <div>
+                        <i className="fa fa-ellipsis-v"></i> Week 1 - INTRO
+                        <span className="float-end">
+                        <i className="fa fa-check-circle text-success"></i>
+                        <i className="fa fa-plus ms-2"></i>
+                        <i className="fa fa-ellipsis-v ms-2"></i>
+                        </span>
+                    </div>
+                    <ul className="list-group">
+                    <li className="list-group-item">
+                        <i className="fa fa-ellipsis-v"></i> LEARNING OBJECIVES
+                        <span className="float-end">
+                        <i className="fa fa-check-circle text-success"></i>
+                        <i className="fa fa-ellipsis-v ms-2"></i>
+                        </span>
+                    </li>
+                        <!-- Inner Loop -->
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Introduction to the course
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Learn what is Web Development
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Creating a development environment
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Creating a Web Application
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Getting started with the 1st assignment
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                    </ul>
+                    <ul className="list-group">
+                    <li className="list-group-item">
+                        <i className="fa fa-ellipsis-v"></i> READING
+                        <span className="float-end">
+                        <i className="fa fa-check-circle text-success"></i>
+                        <i className="fa fa-ellipsis-v ms-2"></i>
+                        </span>
+                    </li>
+                        <!-- Inner Loop -->
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Full Stack Developer - Chapter 1 - Introduction
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Full Stack Developer - Chapter 2 - Creating User Interfaces With HTML
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                    </ul>
+                    <ul className="list-group">
+                    <li className="list-group-item">
+                        <i className="fa fa-ellipsis-v"></i> SLIDES
+                        <span className="float-end">
+                        <i className="fa fa-check-circle text-success"></i>
+                        <i className="fa fa-ellipsis-v ms-2"></i>
+                        </span>
+                    </li>
+                        <!-- Inner Loop -->
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Introduction to Web Development Links to an external site.
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Creating an HTTP server with Node.js Links to an external site.
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Creating a React Application Links to an external site.
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                    </ul>
+                </li>
+                <!-- Week 2 -->
+                <li className="list-group-item">
+                    <div>
+                        <i className="fa fa-ellipsis-v"></i> Week 2 - HTML
+                        <span className="float-end">
+                        <i className="fa fa-check-circle text-success"></i>
+                        <i className="fa fa-plus ms-2"></i>
+                        <i className="fa fa-ellipsis-v ms-2"></i>
+                        </span>
+                    </div>
+                    <ul className="list-group">
+                    <li className="list-group-item">
+                        <i className="fa fa-ellipsis-v"></i> LEARNING OBJECIVES
+                        <span className="float-end">
+                        <i className="fa fa-check-circle text-success"></i>
+                        <i className="fa fa-ellipsis-v ms-2"></i>
+                        </span>
+                    </li>
+                        <!-- Inner Loop -->
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Learn how to create user interfaces with HTML
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Keep working on assigment 1
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Deploy the assignment to Netlify
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                    </ul>
+                    <ul className="list-group">
+                    <li className="list-group-item">
+                        <i className="fa fa-ellipsis-v"></i> READING
+                        <span className="float-end">
+                        <i className="fa fa-check-circle text-success"></i>
+                        <i className="fa fa-ellipsis-v ms-2"></i>
+                        </span>
+                    </li>
+                        <!-- Inner Loop -->
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Full Stack Developer - Chapter 1 - Introduction
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                        <li className="list-group-item">
+                            <i className="fa fa-ellipsis-v"></i> &emsp;&emsp; Full Stack Developer - Chapter 2 - Creating User Interfaces With HTML
+                            <span className="float-end">
+                            <i className="fa fa-check-circle text-success"></i>
+                            <i className="fa fa-ellipsis-v ms-2"></i>
+                            </span>
+                        </li>
+                    </ul>
+                </li>
+            </ul> */}
+            <ModuleList />
+        </div>
+
+        <div className="flex-grow-0 me-2 d-none d-lg-block p-3" style={{width: "250px"}}>
+            {/* // <!-- <h3>Course Status</h3> --> */}
+            <h4>Course Status</h4>
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-light w-100 btn-sm p-2 m-1"><i className="fa fa-ban"></i> Unpublish</button>
+                <button className="btn btn-success w-100 btn-sm p-2 m-1"><i className="fa fa-check-circle text-white"></i> Published</button>
+            </div>
+
+            <div className="pt-4">
+                <button className="btn btn-light w-100 p-2 m-1 text-start"><FaFileImport /> Import Existing Content</button>
+                <button className="btn btn-light w-100 p-2 m-1 text-start"><FaBullseye /> Import From Commons</button>
+                <button className="btn btn-light w-100 p-2 m-1 text-start"><FaBullseye /> Choose Home Page</button>
+                <button className="btn btn-light w-100 p-2 m-1 text-start"><FaChartSimple/> View Course Stream</button>
+                <button className="btn btn-light w-100 p-2 m-1 text-start"><FaBullhorn /> New Announcements</button>
+                <button className="btn btn-light w-100 p-2 m-1 text-start"><FaChartSimple/> New Analytics</button>
+                <button className="btn btn-light w-100 p-2 m-1 text-start"><FaBell/> Course Notifications</button>
+            </div>
+            
+            <div className="pt-4 align-items-center">
+                {/* <!-- <div className="d-flex justify-content-between">
+                    <div>
+                        <p style="font-size: 12px;">Coming Up</p>
+                    </div>
+                    <div className="d-flex align-items-center" style="font-size: 12px;">
+                        <i className="fa fa-calendar"></i>&nbsp;View Calendar
+                    </div>
+                </div> --> */}
+                <div className="row">
+                    <div className="col-6">
+                        <p style={{fontSize: "14px"}}><b>To Do</b></p>
+                    </div>
+                    {/* <div className="col-6" style={{fontSize: "12px"}}>
+                        <i className="fa fa-calendar"></i>&nbsp;View Calendar
+                    </div> */}
+                </div>
+            
+                <hr className="m-2"/>
+
+                <div className="row align-items-top pt-2">
+                    <div className="col-2 text-end">
+                        <p style={{fontSize: "12px"}}><i className="fa fa-calendar"></i></p>
+                    </div>
+                    <div className="col-10" style={{fontSize: "12px"}}>
+                        <span style={{color:"red"}}>Lecture</span><br/>
+                        CS4550.12631.202410<br/>
+                        Sep 7 at 11.45am
+                    </div>
+                </div>
+
+                <div className="row align-items-top pt-2">
+                    <div className="col-2 text-end">
+                        <p style={{fontSize: "12px"}}><i className="fa fa-calendar"></i></p>
+                    </div>
+                    <div className="col-10" style={{fontSize: "12px"}}>
+                        <span style={{color:"red"}}>Lecture</span><br/>
+                        CS4550.12631.202410<br/>
+                        Sep 7 at 11.45am
+                    </div>
+                </div>
+
+                <div className="row align-items-top pt-2">
+                    <div className="col-2 text-end">
+                        <p style={{fontSize: "12px"}}><i className="fa fa-calendar"></i></p>
+                    </div>
+                    <div className="col-10" style={{fontSize: "12px"}}>
+                        <span style={{color:"red"}}>Lecture</span><br/>
+                        CS4550.12631.202410<br/>
+                        Sep 7 at 11.45am
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    // <div>
+    //   <h2>Home</h2>
+      
+    //   <h2>Status</h2>
+    // </div>
   );
 }
 export default Home;
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -5,13 +5,14 @@ import ModuleList from "./Modules/List";
 import CourseNavigation from "./Navigation";
 import Home from "./Home";
 import "./Home/index.css"
-import { FaBars, FaChevronDown } from "react-icons/fa6";
+import { HiMiniBars3 } from "react-icons/hi2";
+import { FaBars, FaChevronDown, FaGlasses } from "react-icons/fa6";
 
 function Courses() {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
   return (
-    <>
+    <div className="container-fluid">
         <div
           className="d-flex d-md-none flex-row p-4 justify-content-between"
           style={{backgroundColor: "black"}}
@@ -30,24 +31,33 @@ function Courses() {
             {/* <i className="fa fa-chevron-down" style={{color: "while"}}></i> */}
           </button>
         </div>
-        <div className="d-none d-md-flex flex-row p-2 align-items-center">
+        {/* Upper Nav */}
+        <div className="d-none d-md-flex flex-row p-2 align-items-center justify-content-between mt-2">
+        <div className="mx-4 d-flex align-items-center">
           <nav aria-label="breadcrumb">
             <ol className="breadcrumb wd-breadcrumb">
               <li>
-                <FaBars className="wd-breadcrumb wd-ham-menu"/>
+                <HiMiniBars3 className="wd-breadcrumb wd-ham-menu" size={30}/>
                 {/* <i className="fa fa-solid fa-bars wd-breadcrumb wd-ham-menu"></i> */}
               </li>
               <li className="breadcrumb-item wd-bc-item">
-                <a href="#">CS5610.11744.202310</a>
+                <a href="">CS5610.11744.202310</a>
               </li>
               <li className="breadcrumb-item active" aria-current="page">
-                <a className="wd-bc-item-active" href="#">Modules</a>
+                <a className="wd-bc-item-active">Modules</a>
               </li>
             </ol>
           </nav>
-          <hr />
+          </div>
+          <div className="mr-4">
+              <button type="button" className="btn btn-outline-secondary">
+                  <FaGlasses /> Student View
+              </button>
+          </div>
         </div>
-      <h2>Courses {course?.name}</h2>
+        <hr />
+        {/* <hr/> */}
+      <h2> Courses {course?.name}</h2>
       <CourseNavigation />
       <div>
         <div
@@ -72,7 +82,7 @@ function Courses() {
       <pre>
         <code>{JSON.stringify(course, null, 2)}</code>
       </pre>
-    </>
+    </div>
   );
 }
 
