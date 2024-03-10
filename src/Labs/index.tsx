25fe45eac67eddaa05fd3f9a9d4d497e0759a3b0
Ronak Vadhaiya <ronak.vadhaiya@somaiya.edu>
2024-03-10 17:19:43 -0400

    Inital redux exercises

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,21 +3,26 @@ import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import { Route, Routes, Link } from 'react-router-dom';
 import Nav from '../Nav';
+// Redux Store
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Labs() {
   return (
-    <div className="container-fluid">
-      <h1>Labs</h1>
-      <Nav />
-      <Link to='./a3'>Assignment 3</Link> | 
-      <Link to='./a4'> Assignment 4</Link>
-      <Routes>
-        <Route path='/a3/*' element={<Assignment3 />} />
-        <Route path='/a4' element={<Assignment4 />} />
-      </Routes>
-      {/* <Assignment3 />
-      <Assignment4 /> */}
-    </div>
+    <Provider store={store}>
+      <div className="container-fluid">
+        <h1>Labs</h1>
+        <Nav />
+        <Link to='./a3'>Assignment 3</Link> | 
+        <Link to='./a4'> Assignment 4</Link>
+        <Routes>
+          <Route path='/a3/*' element={<Assignment3 />} />
+          <Route path='/a4' element={<Assignment4 />} />
+        </Routes>
+        {/* <Assignment3 />
+        <Assignment4 /> */}
+      </div>
+    </Provider>
   );
 }
  export default Labs;
